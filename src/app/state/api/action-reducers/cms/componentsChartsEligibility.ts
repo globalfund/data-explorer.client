30f4eb16d6f56e99018e29a598408065a8c30e7a
Psami-wondah <okechukwusamuel16@gmail.com>
2026-03-05 10:38:48 +0000

    feat: update cms to version 5

--- src/app/state/api/action-reducers/cms/componentsChartsEligibility.ts
+++ src/app/state/api/action-reducers/cms/componentsChartsEligibility.ts
@@ -2,9 +2,7 @@ import { APIModel } from "app/state/api";
 import { CMSApiCallModel } from "app/state/api/interfaces";
 
 const componentsChartsEligibility: CMSApiCallModel = {
-  ...APIModel(
-    `${import.meta.env.VITE_CMS_API}/components-charts-eligibility?locale=all`,
-  ),
+  ...APIModel(`${import.meta.env.VITE_CMS_API}/components-charts-eligibility`),
 };
 
 export default componentsChartsEligibility;
--- src/app/state/api/action-reducers/cms/pagesLocation.ts
+++ src/app/state/api/action-reducers/cms/pagesLocation.ts
@@ -2,37 +2,33 @@ import { APIModel } from "app/state/api";
 import { CMSApiCallModel } from "app/state/api/interfaces";
 
 const pagesLocation: CMSApiCallModel = {
-  ...APIModel(`${import.meta.env.VITE_CMS_API}/pages-location?locale=all`),
+  ...APIModel(`${import.meta.env.VITE_CMS_API}/pages-location`),
 };
 
 export const pagesLocationAccessToFunding: CMSApiCallModel = {
   ...APIModel(
-    `${import.meta.env.VITE_CMS_API}/pages-location-access-to-funding?locale=all`,
+    `${import.meta.env.VITE_CMS_API}/pages-location-access-to-funding`,
   ),
 };
 
 export const pagesLocationGrantImplementation: CMSApiCallModel = {
   ...APIModel(
-    `${import.meta.env.VITE_CMS_API}/pages-location-grant-implementation?locale=all`,
+    `${import.meta.env.VITE_CMS_API}/pages-location-grant-implementation`,
   ),
 };
 
 export const pagesLocationOverview: CMSApiCallModel = {
-  ...APIModel(
-    `${import.meta.env.VITE_CMS_API}/pages-location-overview?locale=all`,
-  ),
+  ...APIModel(`${import.meta.env.VITE_CMS_API}/pages-location-overview`),
 };
 
 export const pagesLocationResourceMobilization: CMSApiCallModel = {
   ...APIModel(
-    `${import.meta.env.VITE_CMS_API}/pages-location-resource-mobilization?locale=all`,
+    `${import.meta.env.VITE_CMS_API}/pages-location-resource-mobilization`,
   ),
 };
 
 export const pagesLocationResults: CMSApiCallModel = {
-  ...APIModel(
-    `${import.meta.env.VITE_CMS_API}/pages-location-results?locale=all`,
-  ),
+  ...APIModel(`${import.meta.env.VITE_CMS_API}/pages-location-results`),
 };
 
 export default pagesLocation;
